d5ef2a77a3cd3e3be2d11194766a9481e3a1236d
Tong Yang <tongy@DN0a22c31e.SUNet>
2018-03-01 18:56:08 -0800

    update stack_tiles

--- code/stack_tiles.m
+++ code/stack_tiles.m
@@ -9,8 +9,8 @@ function output_image = stack_tiles(processed_tile_cell, overlapping_size)
     
     for i = 1:cell_row
         for j = 1:cell_col
-            index_i = i*tileSize;
-            index_j = j*tileSize;
+            index_i = (i-1)*overlapping_size + 1;
+            index_j = (j-1)*overlapping_size + 1;
             output_img(index_i:index_i+16, index_j:index_j+16) = ...
                 output_img(index_i:index_i+16, index_j:index_j+16) + processed_tile_cell{i, j};
             avg_index(index_i:index_i+16, index_j:index_j+16) = ...
