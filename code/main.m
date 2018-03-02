0de3e86c8df56d2e01b509792b1e96ef49d810d0
ThomasChen94 <ThomasChen@DN0a24a825.SUNet>
2018-03-01 19:09:06 -0800

    add main function

---
+++ code/main.m
@@ -0,0 +1,23 @@
+image_path_list = dir('../dataset/*.pgm');
+num_img = length(image_path_list);
+img_list = cell(num_img);
+ 
+for i=1:num_img
+    img_list{i} = im2double(imread(strcat('../dataset/', image_path_list(i).name)));
+end 
+
+
+TILE_SIZE = 16;
+OVERLAPPING_SIZE = 0;
+tiles_list = cell{num_img};
+for i = 1 : num_img
+    I = img_list{i};
+    tiles_list{i} = split_tiles(I, TILE_SIZE, OVERLAPPING_SIZE); 
+end
+merged_cell = merge(tiles_list);
+output_image = stack_tiles(merged_cell);
+imshow(output_image); title('raw image after merging');
+
+
+
+
