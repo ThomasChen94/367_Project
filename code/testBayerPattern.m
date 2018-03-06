ba6bef7568f05093ab39b1cc39a9e480ede81f8d
ThomasChen94 <ThomasChen@DN0a241873.SUNet>
2018-03-05 16:47:56 -0800

    split channel and process

--- code/testBayerPattern.m
+++ code/testBayerPattern.m
@@ -36,6 +36,7 @@ f4 = f4*0.125;
 I = imread('../output/merge_output.pgm');
 img = im2double(I);
 %img = img(1:end-1,:);
+img = output_image;
 [m, n] = size(img);
 red_mask = repmat([1 0;0 0], floor(m/2), floor(n/2));
 green_mask = repmat([0 1;1 0], floor(m/2), floor(n/2));
@@ -77,8 +78,5 @@ blue_highqual = blue_highqual(2:m-1, 2:n-1);
 img_highqual = cat(3, red_highqual, green_highqual, blue_highqual);
 restored_highqual = imadjust(img_highqual,[0,1],[0,1],1.0/2.2);
 
-<<<<<<< HEAD
-imwrite(restored_highqual, 'result.png');
-=======
+
 imwrite(restored_highqual, '../output/test5(rggb).png');
->>>>>>> 12be7375846e7433ad69fc988b03684f3beaef68
